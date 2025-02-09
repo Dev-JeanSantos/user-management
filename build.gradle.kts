bda1c2eaac1909862833b2cfc0f634e4b69fe9b8
Dev-JeanSantos <jeancbsan@gmail.com>
2025-02-09 10:40:38 -0300

    feature: inclusao properties dev e prod e ajustes do banco dde dados h2 e postgresql

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// Banco de dados
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+
+
+	// Testes
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
